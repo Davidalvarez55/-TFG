5d3818b9db93deb6209498ef29a48467c83c81bb
David <alvarez.139973@e.unavarra.es>
2024-02-08 21:13:37 +0100

    Modificacion de iconos y funcionalidad de la barra para navegar

--- app/src/main/java/io/dev/tfg/ui/AdminActivity.kt
+++ app/src/main/java/io/dev/tfg/ui/AdminActivity.kt
@@ -4,13 +4,47 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import io.dev.tfg.R
 
 class AdminActivity : AppCompatActivity() {
+    lateinit var navigation : BottomNavigationView
+
+    private val onNavMenu = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when(item.itemId){
+            R.id.itemUserFrag -> {
+                supportFragmentManager.commit {
+                    replace<AddUserFragment>(R.id.frameContainer)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.itemGraphicsFrag -> {
+                supportFragmentManager.commit {
+                    replace<GraphicsFragment>(R.id.frameContainer)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.itemModifyFrag -> {
+                supportFragmentManager.commit {
+                    replace<ModifyDbFragment>(R.id.frameContainer)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
 
+        navigation = findViewById(R.id.navMenu)
+        navigation.setOnNavigationItemSelectedListener(onNavMenu)
         supportFragmentManager.commit{
             replace<AddUserFragment>(R.id.frameContainer)
             setReorderingAllowed(true)
