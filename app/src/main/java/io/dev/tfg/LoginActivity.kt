61c31bf64de0b2689be4e4c4add6b4c1f3351733
David <alvarez.139973@e.unavarra.es>
2024-02-09 12:08:49 +0100

    Se ha añadido la opcion de añadir nuevos usuarios

--- app/src/main/java/io/dev/tfg/LoginActivity.kt
+++ app/src/main/java/io/dev/tfg/LoginActivity.kt
@@ -3,13 +3,9 @@ package io.dev.tfg
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
-import io.dev.tfg.R
 import io.dev.tfg.ui.AdminActivity
 import io.dev.tfg.ui.MaterialActivity
 
--- app/src/main/java/io/dev/tfg/ui/AddUserFragment.kt
+++ app/src/main/java/io/dev/tfg/ui/AddUserFragment.kt
@@ -5,56 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.firestore.FirebaseFirestore
 import io.dev.tfg.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AddUserFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddUserFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var user: EditText
+    private lateinit var buttonUser: Button
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_user, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_add_user, container, false)
+
+        user = rootView.findViewById(R.id.User)
+        buttonUser = rootView.findViewById(R.id.UserButton)
+
+        buttonUser.setOnClickListener{
+            addUserToFirebase()
+        }
+
+        return rootView
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddUserFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddUserFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun addUserToFirebase(){
+        val userName = user.text.toString()
+        val password = "1234567"
+
+        if(userName.isNotEmpty()){
+            val pass = hashMapOf("password" to password)
+            db.collection("Usuarios").document(userName)
+                .set(pass)
+                .addOnSuccessListener {  }
+                .addOnFailureListener {  }
+        } else {
+            user.error = "Nombre de usuario no puede estar vacio"
+        }
     }
 }
