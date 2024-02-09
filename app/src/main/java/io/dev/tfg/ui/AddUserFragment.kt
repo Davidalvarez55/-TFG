9a8fc2709f88bfa1ca904a8c8f4e64a3f4f4f138
David <alvarez.139973@e.unavarra.es>
2024-02-09 12:17:23 +0100

    mensaje de confirmacion de usuario

--- app/src/main/java/io/dev/tfg/ui/AddUserFragment.kt
+++ app/src/main/java/io/dev/tfg/ui/AddUserFragment.kt
@@ -7,14 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import com.google.firebase.firestore.FirebaseFirestore
 import io.dev.tfg.R
 
 class AddUserFragment : Fragment() {
     private lateinit var user: EditText
     private lateinit var buttonUser: Button
+    private lateinit var confirmation : TextView
 
     private val db = FirebaseFirestore.getInstance()
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +29,7 @@ class AddUserFragment : Fragment() {
 
         user = rootView.findViewById(R.id.User)
         buttonUser = rootView.findViewById(R.id.UserButton)
+        confirmation = rootView.findViewById(R.id.confirmation)
 
         buttonUser.setOnClickListener{
             addUserToFirebase()
@@ -41,8 +46,8 @@ class AddUserFragment : Fragment() {
             val pass = hashMapOf("password" to password)
             db.collection("Usuarios").document(userName)
                 .set(pass)
-                .addOnSuccessListener {  }
-                .addOnFailureListener {  }
+                .addOnSuccessListener { confirmation.setText("usuario añadido correctamente") }
+                .addOnFailureListener { confirmation.setText("usuario no añadido correctamente") }
         } else {
             user.error = "Nombre de usuario no puede estar vacio"
         }
