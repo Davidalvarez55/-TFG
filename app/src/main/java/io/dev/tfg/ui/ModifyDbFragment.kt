9e95e0482bf4903a36a87fb0e9d024c9f6dcc41c
David <alvarez.139973@e.unavarra.es>
2024-02-13 19:48:14 +0100

    Añadida la funcionalidad de modificar los horarios de fichaje

--- app/src/main/java/io/dev/tfg/ui/ModifyDbFragment.kt
+++ app/src/main/java/io/dev/tfg/ui/ModifyDbFragment.kt
@@ -5,56 +5,101 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import io.dev.tfg.R
+import java.text.SimpleDateFormat
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ModifyDbFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ModifyDbFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var user: EditText
+    private lateinit var singHourText: EditText
+    private lateinit var leaveHourText: EditText
+    private lateinit var search: Button
+    private lateinit var change: Button
 
+    private val db = FirebaseFirestore.getInstance()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_modify_db, container, false)
-    }
+        val rootView = inflater.inflate(R.layout.fragment_modify_db, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ModifyDbFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ModifyDbFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        val todayDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
+        user = rootView.findViewById(R.id.user)
+        singHourText = rootView.findViewById(R.id.singHour)
+        leaveHourText = rootView.findViewById(R.id.leaveHour)
+        search = rootView.findViewById(R.id.search)
+        change = rootView.findViewById(R.id.change)
+
+        search.setOnClickListener {
+            val userId = user.text.toString()
+            val documentUserRef = db
+                .collection("fichajes")
+                .document(todayDate)
+                .collection("usuarios")
+                .document(userId)
+            documentUserRef.get()
+                .addOnSuccessListener { documentSnapshot ->
+                    if(documentSnapshot.exists()){
+                        val singHour = documentSnapshot.getString("fichaje_entrada")
+                        val leaveHour = documentSnapshot.getString("fichaje_salida")
+
+                        singHourText.setText(singHour)
+                        leaveHourText.setText(leaveHour)
+
+                        singHourText.visibility = View.VISIBLE
+                        leaveHourText.visibility = View.VISIBLE
+
+                        singHourText.isEnabled = true
+                        leaveHourText.isEnabled = true
+
+                        search.visibility = View.INVISIBLE
+                        search.isClickable = false
+
+                        change.visibility = View.VISIBLE
+                        change.isClickable = true
+                    }
                 }
-            }
+        }
+        change.setOnClickListener{
+            val userId = user.text.toString()
+            val singHourNew = singHourText.text.toString()
+            val leaveHourNew = leaveHourText.text.toString()
+            val update = hashMapOf<String, Any>(
+                "fichaje_entrada" to singHourNew,
+                "fichaje_salida" to leaveHourNew,
+                "modificado" to true
+            )
+            db.collection("fichajes")
+                .document(todayDate)
+                .collection("usuarios")
+                .document(userId)
+                .update(update)
+                .addOnSuccessListener {
+                    Toast.makeText(context, "Datos actualizados exitosamente", Toast.LENGTH_SHORT).show()
+                }
+            user.setText("")
+            singHourText.setText("")
+            leaveHourText.setText("")
+
+            singHourText.visibility = View.INVISIBLE
+            leaveHourText.visibility = View.INVISIBLE
+
+            singHourText.isEnabled = false
+            leaveHourText.isEnabled = false
+
+            search.visibility = View.VISIBLE
+            search.isClickable = true
+
+            change.visibility = View.INVISIBLE
+            change.isClickable = false
+
+        }
+        return rootView
     }
 }
