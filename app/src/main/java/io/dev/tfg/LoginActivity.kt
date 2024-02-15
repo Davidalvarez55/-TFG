cf195f6b346df734911da0e8310cd98abdcddaf8
David <alvarez.139973@e.unavarra.es>
2024-02-15 20:50:29 +0100

    Se han añadido nuevos atributos a los usuarios

--- app/src/main/java/io/dev/tfg/LoginActivity.kt
+++ app/src/main/java/io/dev/tfg/LoginActivity.kt
@@ -55,7 +55,6 @@ class LoginActivity : AppCompatActivity() {
                         }
                     }
                 } else {
-                    // Tratar caso donde el documento no existe
                 }
             }
         }
--- app/src/main/java/io/dev/tfg/ui/AddUserFragment.kt
+++ app/src/main/java/io/dev/tfg/ui/AddUserFragment.kt
@@ -5,10 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Switch
-import android.widget.TextView
+import android.widget.*
+import android.widget.AdapterView
 import com.google.firebase.firestore.FirebaseFirestore
 import io.dev.tfg.R
 import io.dev.tfg.classes.Singing
@@ -16,9 +14,14 @@ import java.security.SecureRandom
 
 class AddUserFragment : Fragment() {
     private lateinit var user: EditText
+    private lateinit var surname: EditText
+    private lateinit var surname2: EditText
+    private lateinit var id: EditText
     private lateinit var buttonUser: Button
     private lateinit var confirmation : TextView
     private lateinit var admin: Switch
+    private lateinit var hours: Spinner
+    private lateinit var hour : String
 
     private val db = FirebaseFirestore.getInstance()
 
@@ -31,9 +34,25 @@ class AddUserFragment : Fragment() {
         val rootView = inflater.inflate(R.layout.fragment_add_user, container, false)
 
         user = rootView.findViewById(R.id.User)
+        surname = rootView.findViewById(R.id.surname)
+        surname2 = rootView.findViewById(R.id.surname2)
+        id = rootView.findViewById(R.id.id)
         buttonUser = rootView.findViewById(R.id.UserButton)
         confirmation = rootView.findViewById(R.id.confirmation)
         admin = rootView.findViewById(R.id.adminSwitch)
+        hours = rootView.findViewById(R.id.spinner)
+        val options = resources.getStringArray(R.array.hours)
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+
+        hours.adapter =  adapter
+        hours.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                hour = options[position]
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {
+            }
+        }
 
         buttonUser.setOnClickListener{
             addUserToFirebase()
@@ -49,9 +68,13 @@ class AddUserFragment : Fragment() {
 
         if(userName.isNotEmpty()){
             val pass = hashMapOf(
+                "Primer Apellido" to surname,
+                "Segundo Apellido" to surname2,
+                "Dni" to id,
                 "password" to password,
                 "Admin" to switchAdmin,
-                "fichado" to false
+                "fichado" to false,
+                "hora_entrada" to hour
             )
             db.collection("Usuarios").document(userName)
                 .set(pass)
